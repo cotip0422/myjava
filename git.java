21a78d415b2b0e977980392a4fb24ac481d45c2f
cotip0422 <cotip0422@gmail.com>
2021-06-28 20:01:24 +0900

    two

--- git.java
+++ git.java
@@ -3,5 +3,8 @@ package test;
 public class git {
     public static void main(String[] args) {
         System.out.println("test");
+        String git;
+        git = "good";
+        System.out.println(git);
     }
 }
